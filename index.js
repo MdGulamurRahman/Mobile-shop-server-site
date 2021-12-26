7c3f60a787949854d05a003fc9da1700fddff9fa
Abrarhusamfahim <fahimjahin321@gmail.com>
2021-12-26 20:48:46 +0600

    added new codes

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", async (req, res) => {
-  res.send("server is running!");
-});
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jsdem.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -142,4 +140,7 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("server is running!");
+});
 app.listen(port, () => console.log(`server is running on port ${port}`));
